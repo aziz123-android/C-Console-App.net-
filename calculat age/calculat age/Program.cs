1179fea89a2202056bf20f2a103d95bde7666a21
Abdul Aziz <aadiazp7@gmail.com>
2024-11-01 00:55:55 +0500

    Add files via upload

---
+++ calculat age/calculat age/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculat_age
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the current year: ");
+            int currentYear = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter your birth year: ");
+            int birthYear = int.Parse(Console.ReadLine());
+
+            // Create an AgeCalculator instance with user input
+            AgeCalculator ageCalculator = new AgeCalculator(currentYear, birthYear);
+            int age = ageCalculator.CalculateAge();
+
+            Console.WriteLine($"Your current age is: {age}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ calculat age/calculat age/class.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculat_age
+{
+  
+        internal class AgeCalculator
+        {
+            private int currentYear;
+            private int birthYear;
+
+            // Constructor to initialize currentYear and birthYear
+            public AgeCalculator(int currentYear, int birthYear)
+            {
+                this.currentYear = currentYear;
+                this.birthYear = birthYear;
+            }
+
+            // Method to calculate age
+            public int CalculateAge()
+            {
+                return currentYear - birthYear;
+            }
+        }
+}
